4afdd4d03494d006a9bc02d3bbdb64ba4c86f41f
Jinbo Xing <Doubiiu@users.noreply.huggingface.co>
2023-12-04 09:34:13 +0000

    Update app.py

--- app.py
+++ app.py
@@ -104,7 +104,8 @@ i2v_examples = [
     ['prompts/dance1.jpeg', 'two people dancing', 50, 7.5, 1.0, 3, 116],
     ['prompts/fire_and_beach.jpg', 'a campfire on the beach and the ocean waves in the background', 50, 7.5, 1.0, 3, 111],
     ['prompts/girl3.jpeg', 'girl talking and blinking', 50, 7.5, 1.0, 3, 111],
-    ['prompts/guitar0.jpeg', 'bear playing guitar happily, snowing', 50, 7.5, 1.0, 3, 111],
+    ['prompts/guitar0.jpeg', 'bear playing guitar happily, snowing', 50, 7.5, 1.0, 3, 122],
+    ['prompts/surf.png', 'a man is surfing', 50, 7.5, 1.0, 3, 123],
 ]
 css = """#input_img {max-width: 256px !important} #output_vid {max-width: 256px; max-height: 256px}"""
 
